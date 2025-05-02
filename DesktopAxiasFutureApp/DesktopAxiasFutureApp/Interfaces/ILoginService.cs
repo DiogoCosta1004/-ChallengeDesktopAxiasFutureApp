11c944f551a631e968c25a1203e6bf7c4434b95e
DiogoCosta1004 <121439395+DiogoCosta1004@users.noreply.github.com>
2025-05-02 20:41:09 -0300

    fix: adjustment

--- DesktopAxiasFutureApp/DesktopAxiasFutureApp/Interfaces/ILoginService.cs
+++ DesktopAxiasFutureApp/DesktopAxiasFutureApp/Interfaces/ILoginService.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace DesktopAxiasFutureApp.Interfaces
+﻿namespace DesktopAxiasFutureApp.Interfaces
 {
     public interface ILoginService
     {
--- DesktopAxiasFutureApp/DesktopAxiasFutureApp/Interfaces/IWebSocketService.cs
+++ DesktopAxiasFutureApp/DesktopAxiasFutureApp/Interfaces/IWebSocketService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace DesktopAxiasFutureApp.Interfaces
+﻿namespace DesktopAxiasFutureApp.Interfaces
 {
     public interface IWebSocketService : IDisposable
     {
